895187be282f0223190b2d268bef81010d769356
Sathya Vakacharla <sathyavakacharla@Sathyas-MacBook-Air.local>
2026-07-20 23:44:56 +0100

    langchain messages classes added to keep persistent memory inline with langchain builtin classes

--- Chatbot.py
+++ Chatbot.py
@@ -1,4 +1,5 @@
 from langchain_openrouter import ChatOpenRouter
+from langchain.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 import os
 
@@ -6,15 +7,19 @@ load_dotenv(override=True)
 
 model = ChatOpenRouter(model=os.environ.get("MODEL"))   
 
-conversation_history = []
+conversation_history = [
+    SystemMessage(content="""
+    You are a helpful AI assistant who answers queries with a bit of humour. Use emojis to make your response looks attractive. Give friendly answers while maintaining the professional tone.
+    """)
+]
 
 while True:
     user_input = input("You: ")
-    conversation_history.append(user_input)
+    conversation_history.append(HumanMessage(content=user_input))
     if user_input.lower() in ["quit", "exit"]:
         break
     ##usually we will give user_input here but we are giving conversation history to maintain context in the conversation
     response = model.invoke(conversation_history) 
-    conversation_history.append(response.content)
+    conversation_history.append(AIMessage(content=response.content))
     print(response.content)
 print(f"Conversation History: {conversation_history}")
